303168a0e3b8da541a8d828c86f511ad2d58ccff
alex <alexanderstapp@gmail.com>
2021-05-06 10:14:02 -0600

    styling

--- responsive-app/src/App.js
+++ responsive-app/src/App.js
@@ -1,21 +1,44 @@
 import './App.css';
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className='header'>
-        <h1>Title Here</h1>
-        <h4 className='button'>MENU</h4>
-        <nav className='nav-bar'>
-          <h4>SERVICES</h4>
-          <h4>PORTFOLIO</h4>
-          <h4>ABOUT</h4>
-          <h4>TEAM</h4>
-          <h4>CONTACT</h4>
-        </nav>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({showMenu: !this.state.showMenu})
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className='header'>
+          <h1>Title Here</h1>
+          <nav>
+            <ul className='nav-main'>
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+            <button onClick={this.toggleMenu}>BURGER</button>
+            <ul className={`nav-drop ${this.state.showMenu ? 'show' : ''}`}>
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
